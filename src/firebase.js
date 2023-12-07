24cbbbc29da374fc64f7569adefaeccdbd8870f3
JB <drv98@naver.com>
2023-12-07 09:28:42 +0900

    파이어베이스 설정 수정

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,6 @@
 import { initializeApp } from 'firebase/app';
+import { GoogleAuthProvider, getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
@@ -8,5 +10,11 @@ const firebaseConfig = {
   messagingSenderId: '737866606655',
   appId: '1:737866606655:web:cb1de7d4922c276342f08a',
 };
-
+//파이어베이스 초기화
 const app = initializeApp(firebaseConfig);
+//구글인증과 파이어스토어DB
+const auth = getAuth();
+const googleAuth = new GoogleAuthProvider();
+const db = getFirestore();
+
+export { auth, googleAuth, db };
